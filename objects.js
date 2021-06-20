0193492da09f1c8147d158d0622ddc33c3fc5c88
Verboddo <verboddo@gmail.com>
2021-06-20 11:25:46 +0200

    logged object and adjusted values from object

--- objects.js
+++ objects.js
@@ -26,13 +26,14 @@ const product = {
 };
 
 // 3a. Log de naam van de serie in de terminal.
-
+console.log(product.productName)
 // 3b. Overschrijf de oude waarde van de property color ("creme") naar "roze". Log het object in de terminal om te checken of het gelukt is.
-
+product.color = 'roze'
+console.log(product.color)
 // 3c. Log de tweede materiaalsoort ("kunststof") in de terminal
-
+console.log(product.material[1])
 // 1d. Log de afmetingen (b-h-d) in de terminal
-
+console.log(product["b-h-d"])
 
 // === Opdracht 4 ===
 // Declareer een variabele met daarin een string die het product beschrijft. Gebruik hiervoor de properties productName en material uit het object.
